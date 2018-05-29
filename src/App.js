fb422d546c7ca76b01204bd42bbe1cc3106e17fc
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-05-29 14:47:55 -0700

    Copied and cloned repo. Started parsing out the names for the app title

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import chatMessages from './data/messages.json';
 
 class App extends Component {
   render() {
-    console.log(chatMessages);
+
+    const senderVladimir = chatMessages[0]["sender"];
+    const senderEstragon = chatMessages[1]["sender"];
+    const messageData = chatMessages;
+
+    console.log(messageData);
 
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Application title</h1>
+          <h1 className="App-title">Chat between {senderVladimir} and {senderEstragon}</h1>
         </header>
         <main className="App-main">
         </main>
